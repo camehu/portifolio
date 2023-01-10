b8ba995c572b023f289c82381da2766dd1c4cfbb
camehu <camehu@gmail.com>
2023-01-10 15:52:41 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h4>Bem vindo ao meu Portifolio</h4>
+     <h2>Bem vindo ao meu Portifolio</h2>
     </div>
   );
 }
