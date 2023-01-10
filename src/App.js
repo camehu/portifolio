92d7bb66bcea3abb2b315d4524fc16216de19d12
camehu <camehu@gmail.com>
2023-01-10 16:29:07 -0300

    up-app-portifolio

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import NavBar from './companents/header/NavBar'
 function App() {
   return (
     <div className="App">
-      <h2>Me chamos Carlos Humberto</h2>
       <NavBar/>
       
     </div>
