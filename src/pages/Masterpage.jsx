ef77ad2f783f096c7f7a28df56956c1b63e7d8fa
camehu <camehu@gmail.com>
2023-02-01 12:29:58 -0300

    up-foto-css

--- src/pages/Masterpage.jsx
+++ src/pages/Masterpage.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/alt-text */
-import React, {useState} from 'react'
-import '../pages/masterPage.module.css'
+import React, {useState} from 'react';
+import '../pages/masterPage.module.css';
+import humberto from '../companents/img/Carlos-humberto.jpg'
+
 
 
 
@@ -13,11 +15,10 @@ const Masterpage = () => {
    
     <div style={{padding:'3rem'}}>   
 
-    <div class="d-flex justify-content-center align-content-center flex-wrap">
-       
+    <div class="d-flex justify-content-center align-content-center flex-wrap">         
     
        <div id='container' style={{padding:'3rem', marginLeft:'20px'}}>             
-       <img src="https://yesofcorsa.com/wp-content/uploads/2017/10/Sylvester-Stallone-Wallpaper-For-IPhone-Download.jpg" alt="Nature" class="responsive" width='310px' style={{boxShadow:'5px 5px 7px 7px #fff'}}/> 
+       <img src={humberto} alt="Nature" class="responsive" width='285px' style={{boxShadow:'5px 5px 7px 7px #fff'}}/> 
          </div>
       
 
@@ -33,12 +34,13 @@ const Masterpage = () => {
 
        </p>
 
-       <div className="modal-footer">
+       <div style={{display: 'inline-block'}}>
+       <button type="button" class="btn btn-outline-warning" onClick={() => setCount(count + 1)} style={{width:'200px',height:'50px',borderRadius:'40px', marginLeft:'4.5rem'}}>Projetos</button>
+       <button type="button" class="btn btn-outline-warning" onClick={() => setCount(count + 1)} style={{width:'200px',height:'50px',borderRadius:'40px', margin:'2rem'}}>Contato</button>
 
        </div>
         
-        <button type="button" class="btn btn-outline-warning" onClick={() => setCount(count + 1)} style={{width:'200px',height:'50px', marginLeft:'190px',marginTop:'2rem',borderRadius:'40px', float:'left'}}>Projetos</button>
-       <button type="button" class="btn btn-outline-warning" onClick={() => setCount(count + 1)} style={{width:'200px',height:'50px', marginLeft:'190px',marginTop:'2rem',borderRadius:'40px', float:'left'}}>Contato</button>
+       
 
        
         </div>
